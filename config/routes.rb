21ea5f7287cb3e0df078335ea87463c8412d3c88
Rishav Kumar <rishav.kumar@anchanto.com>
2021-02-16 21:45:38 +0530

    Added Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
   root 'public#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
